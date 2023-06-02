c5d14eeda0c92c43e15df1619f50d2f47df18585
olgaobraztsova <olga.obraztsova@gmail.com>
2023-06-03 00:08:01 +0530

    style: method definitions separated by a separate line in FilmStorage and UserStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -6,8 +6,12 @@ import java.util.Collection;
 
 public interface FilmStorage {
     Collection<Film> getAllFilms();
+
     Film create(Film film);
+
     Film update(Film film);
+
     Boolean delete(Film film);
+
     Film getFilmById(Integer id);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -6,8 +6,12 @@ import java.util.Collection;
 
 public interface UserStorage {
     Collection<User> getAllUsers();
+
     User create(User user);
+
     User update(User user);
+
     Boolean delete(User user);
+
     User getUserById(Integer id);
 }
