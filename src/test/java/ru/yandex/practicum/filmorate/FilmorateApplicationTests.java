c5ba5d55b61097611987368d92a9bec6d43e25e0
olgaobraztsova <olga.obraztsova@gmail.com>
2023-06-22 00:30:01 +0530

    fix style: removed unused import and whitespaces

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -18,7 +18,6 @@ import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
@@ -27,98 +26,98 @@ import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TES
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class FilmorateApplicationTests {
 
-	@Autowired
-	@Qualifier("filmDbStorage") FilmStorage filmStorage;
+    @Autowired
+    @Qualifier("filmDbStorage")
+    FilmStorage filmStorage;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
-	@Test
-	public void testGetFilmById() {
-		Film film = filmStorage.getFilmById(1);
-		assertThat(film).hasFieldOrPropertyWithValue("id", 1);
-	}
+    @Test
+    public void testGetFilmById() {
+        Film film = filmStorage.getFilmById(1);
+        assertThat(film).hasFieldOrPropertyWithValue("id", 1);
+    }
 
-	@Test
-	public void testGetAllFilms() {
-		Collection<Film> films = filmStorage.getAllFilms();
-		assertThat(films.size()).isEqualTo(5);
-	}
+    @Test
+    public void testGetAllFilms() {
+        Collection<Film> films = filmStorage.getAllFilms();
+        assertThat(films.size()).isEqualTo(5);
+    }
 
+    @Test
+    public void testCreateFilm() {
+        Film expectedFilm = new Film(6,
+                "Film name",
+                "description",
+                LocalDate.of(2020, 10, 5),
+                120,
+                new HashSet<Integer>(),
+                new HashSet<Genre>(),
+                new Mpa(1, "Комедия"));
+        Film actualFilm = filmStorage.create(expectedFilm);
+        assertThat(actualFilm).usingRecursiveComparison()
+                .ignoringFields("filmId").isEqualTo(expectedFilm);
+    }
 
-	@Test
-	public void testCreateFilm() {
-		Film expectedFilm = new Film (6,
-				"Film name",
-				"description",
-				LocalDate.of(2020, 10, 5),
-				120,
-				new HashSet<Integer>(),
-				new HashSet<Genre>(),
-				new Mpa(1, "Комедия"));
-		Film actualFilm = filmStorage.create(expectedFilm);
-		assertThat(actualFilm).usingRecursiveComparison()
-				.ignoringFields("filmId").isEqualTo(expectedFilm);
-	}
+    @Test
+    public void testUpdateFilm() {
+        Film expectedFilm = new Film(1,
+                "Film name",
+                "description",
+                LocalDate.of(2020, 10, 5),
+                120,
+                new HashSet<Integer>(),
+                new HashSet<Genre>(),
+                new Mpa(1, "G"));
 
-	@Test
-	public void testUpdateFilm() {
-		Film expectedFilm = new Film (1,
-				"Film name",
-				"description",
-				LocalDate.of(2020, 10, 5),
-				120,
-				new HashSet<Integer>(),
-				new HashSet<Genre>(),
-				new Mpa(1, "G"));
+        Film actualFilm = filmStorage.update(expectedFilm);
+        assertThat(actualFilm).usingRecursiveComparison()
+                .ignoringFields("filmId").isEqualTo(expectedFilm);
+    }
 
-		Film actualFilm = filmStorage.update(expectedFilm);
-		assertThat(actualFilm).usingRecursiveComparison()
-				.ignoringFields("filmId").isEqualTo(expectedFilm);
-	}
+    @Test
+    public void testAddLike() {
+        HashSet<Integer> expectedLikes = new HashSet<>(Arrays.asList(1, 2, 3, 4));
 
-	@Test
-	public void testAddLike() {
-		HashSet<Integer> expectedLikes = new HashSet<>(Arrays.asList(1, 2, 3, 4));
+        Film expectedFilm = new Film(1,
+                "Inception",
+                "description",
+                LocalDate.of(2010, 6, 8),
+                148,
+                expectedLikes,
+                new HashSet<Genre>(),
+                new Mpa(3, "PG-13"));
 
-		Film expectedFilm = new Film (1,
-				"Inception",
-				"description",
-				LocalDate.of(2010, 6, 8),
-				148,
-				expectedLikes,
-				new HashSet<Genre>(),
-				new Mpa(3, "PG-13"));
+        filmStorage.addLike(1, 4);
+        Film actualFilm = filmStorage.getFilmById(1);
+        assertThat(actualFilm).usingRecursiveComparison()
+                .ignoringFields("filmId", "description", "genres").isEqualTo(expectedFilm);
+    }
 
-		filmStorage.addLike(1, 4);
-		Film actualFilm = filmStorage.getFilmById(1);
-		assertThat(actualFilm).usingRecursiveComparison()
-				.ignoringFields("filmId", "description", "genres").isEqualTo(expectedFilm);
-	}
+    @Test
+    public void testRemoveLike() {
+        HashSet<Integer> expectedLikes = new HashSet<>(Arrays.asList(1, 2));
 
-	@Test
-	public void testRemoveLike() {
-		HashSet<Integer> expectedLikes = new HashSet<>(Arrays.asList(1, 2));
+        Film expectedFilm = new Film(1,
+                "Inception",
+                "description",
+                LocalDate.of(2010, 6, 8),
+                148,
+                expectedLikes,
+                new HashSet<Genre>(),
+                new Mpa(3, "PG-13"));
 
-		Film expectedFilm = new Film (1,
-				"Inception",
-				"description",
-				LocalDate.of(2010, 6, 8),
-				148,
-				expectedLikes,
-				new HashSet<Genre>(),
-				new Mpa(3, "PG-13"));
+        filmStorage.removeLike(1, 3);
+        Film actualFilm = filmStorage.getFilmById(1);
+        assertThat(actualFilm).usingRecursiveComparison()
+                .ignoringFields("filmId", "description", "genres").isEqualTo(expectedFilm);
+    }
 
-		filmStorage.removeLike(1, 3);
-		Film actualFilm = filmStorage.getFilmById(1);
-		assertThat(actualFilm).usingRecursiveComparison()
-				.ignoringFields("filmId", "description", "genres").isEqualTo(expectedFilm);
-	}
-
-	@Test
-	public void testGetMostPopularFilms() {
-		List<Film> mostPopularFilms = filmStorage.getMostPopularFilms(1);
-		assertEquals("Inception", mostPopularFilms.stream().findAny().get().getName());
-	}
+    @Test
+    public void testGetMostPopularFilms() {
+        List<Film> mostPopularFilms = filmStorage.getMostPopularFilms(1);
+        assertEquals("Inception", mostPopularFilms.stream().findAny().get().getName());
+    }
 }
